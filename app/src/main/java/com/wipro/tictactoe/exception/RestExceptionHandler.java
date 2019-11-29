00474f9159b19a9a96cef64ca22a36e45a3bbcb5
learntechpuzz <learntechpuzz@gmail.com>
2019-11-29 22:02:19 +0530

    change

--- app/src/main/java/com/wipro/tictactoe/exception/RestExceptionHandler.java
+++ app/src/main/java/com/wipro/tictactoe/exception/RestExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.wipro.tictactoe.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.wipro.tictactoe.response.ApiError;
@@ -11,16 +14,50 @@ import com.wipro.tictactoe.response.ApiError;
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+	/**
+	 * Handle required params validation exception
+	 */
+	@Override
+	public ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+	}
+
+	/**
+	 * Handle player already exists exception
+	 * 
+	 * @param ex
+	 * @return
+	 */
 	@ExceptionHandler(PlayerAlreadyExistsException.class)
 	public ResponseEntity<Object> handlePlayerAlreadyExistsException(PlayerAlreadyExistsException ex) {
 		ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
-		return new ResponseEntity(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
 	}
 
+	/**
+	 * Handle move not all allowed exception
+	 * 
+	 * @param ex
+	 * @return
+	 */
 	@ExceptionHandler(MoveNotAllowedException.class)
 	public ResponseEntity<Object> handleMoveNotAllowedException(MoveNotAllowedException ex) {
 		ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
-		return new ResponseEntity(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+	}
+
+	/**
+	 * Handle all exception
+	 * 
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleAllExceptions(Exception ex) {
+		ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
 	}
 
 }
